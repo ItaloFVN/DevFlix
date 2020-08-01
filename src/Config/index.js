b35a3bcb67a9c1ab69bd6b224d926df6702ba045
Italo Nascimento <italo.nascimento@dtidigital.com.br>
2020-08-01 19:22:41 -0300

    Correção da url do heroku

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://you-flix.herokuapp.com/';
+  : 'https://you-flix.herokuapp.com';
 
 export default {
   URL_BACKEND,
